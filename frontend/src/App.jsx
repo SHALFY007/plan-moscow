cbfc9e539648ea5860ea7b30df90d4903142ad21
vladislav <vl_evlashin@mail.ru>
2023-11-02 14:49:52 +0300

    init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1 className="main_header">Генератор заданий <br /> по информатике</h1>
+        <h3 className='main_description'>Тренажер , который поможет вам подготовиться к экзаменам и самостоятельным работам по информатике</h3>
+        <button className="start_btn">
+          <span>Начать</span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
